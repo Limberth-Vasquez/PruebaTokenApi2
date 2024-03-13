8407efefd497e92d226a2c797db82c594dddc067
Limberth Vasquez Quesada <lvasquez@tecnosys.net>
2024-03-12 23:18:58 -0600

    Add project files.

---
+++ PruebaTokenApi2/Models/UserMaster.cs
@@ -0,0 +1,13 @@
+﻿namespace PruebaTokenApi2.Models
+{
+	public class UserMaster
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserRoles { get; set; }
+        public string UserEmailID { get; set; }
+
+        public UserMaster() { }
+    }
+}
---
+++ PruebaTokenApi2/Models/UserMasterRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTokenApi2.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        // SECURITY_DBEntities it is your context class
+        //SECURITY_DBEntities context = new SECURITY_DBEntities();
+        //This method is used to check and validate the user credentials
+
+
+        readonly List<UserMaster> context = new List<UserMaster>()
+        {
+            new UserMaster()
+            {
+                UserID = 101,
+                UserName = "lvasquez",
+                UserPassword = "1234",
+                UserRoles = "Admin",
+                UserEmailID = "lvasquez@prides.net"
+            },
+            new UserMaster()
+            {
+                UserID = 102,
+                UserName = "jsandoval",
+                UserPassword = "1234",
+                UserRoles = "SuperAdmin",
+                UserEmailID = "jsandoval@prides.net"
+            },
+        };
+
+		public void Dispose()
+		{
+		}
+
+		public UserMaster ValidateUser(string username, string password)
+        {
+            return context.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.UserPassword == password);
+        }
+        /*public void Dispose()
+        {
+            context.Dispose();
+        }*/
+    }
+}
